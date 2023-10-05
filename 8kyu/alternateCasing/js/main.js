fe834983063acb06699d3812b302dc49272df634
Derin DeNio <derindenio@gmail.com>
2023-10-04 19:19:25 -0600

    Solved Codewars 'alternate casing' problem

---
+++ 8kyu/alternateCasing/js/main.js
@@ -0,0 +1,26 @@
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; 
+// see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+function toAlternatingCase(str) {
+    let newStr = '';
+    for (let i = 0; i < str.length; i++){
+        if ( str[i] == str[i].toUpperCase()) {
+            newStr+=str[i].toLowerCase();
+        }else {
+            newStr+=str[i].toUpperCase();
+        }
+    }return newStr
+}
+
+// toAlternatingCase('hello world')
+console.log(toAlternatingCase('1a2bBEr3C'))
