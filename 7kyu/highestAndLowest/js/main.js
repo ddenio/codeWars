e3769d1a1420d901f95d035888d80a54d3508689
Derin DeNio <derindenio@gmail.com>
2023-11-06 16:20:38 -0700

    Solved Codewars 'Highest and Lowest' problem

---
+++ 7kyu/highestAndLowest/js/main.js
@@ -0,0 +1,21 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+    numbers=numbers.split(' ').map(Number);
+    console.log(numbers)
+    numbers = numbers.sort(function(a,b){ return b-a});
+    console.log(numbers)
+    return numbers[0].toString() + " " + numbers[numbers.length-1].toString()
+    
+  }
+
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
