69a01421381feafea0578e7d1dc368797492c93b
Derin DeNio <derindenio@gmail.com>
2023-12-21 15:18:30 -0700

    Solved Codewars 'Unique in Order' Problem

---
+++ 6kyu/uniqueInOrder/js/main.js
@@ -0,0 +1,29 @@
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements 
+// with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+function uniqueInOrder(iterable){
+    if (!Array.isArray(iterable)) {
+        iterable = iterable.split('');
+        console.log(iterable)
+    }
+    let result = [];
+    for (let i = 0; i < iterable.length; i++) {
+        if (i === 0 || iterable[i] !== iterable[i - 1]) {
+            result.push(iterable[i]);
+        }
+    }return result
+}
+
+console.log(uniqueInOrder('AAABBBBCCCDDDAAABBB'))
+
+//top codewars solution:
+
+// var uniqueInOrder=function(iterable){
+//     return [...iterable].filter((a, i) => a !== iterable[i-1])
+// }
