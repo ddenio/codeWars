812675b13f6a458c3bafae73de3d04197c50634f
Derin DeNio <derindenio@gmail.com>
2024-01-10 17:52:20 -0700

    Added top Codewars solution to example

--- 8kyu/wolfInSheeps/js/main.js
+++ 8kyu/wolfInSheeps/js/main.js
@@ -29,3 +29,10 @@ function warnTheSheep(queue) {
 console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]))
 console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]))
 console.log(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"]))
+
+//top codewars solutuion:
+
+// function warnTheSheep(queue) {
+//     const position = queue.reverse().indexOf('wolf');
+//     return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+//   }
