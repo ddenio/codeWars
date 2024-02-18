974e28b9c202f8b03b9016c04760200cb7f5d3a3
Derin DeNio <derindenio@gmail.com>
2024-02-17 19:50:25 -0700

    Solved Codewars 'Bin to Decimal' problem

---
+++ 8kyu/binToDecimal/js/main.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin) {
+    return parseInt(bin, 2)
+  }
+
+console.log(binToDec('10'))
+console.log(binToDec('1001001'))
