be068b74073ed214a378db8c5ce1b21f7d3a6047
Derin DeNio <derindenio@gmail.com>
2024-03-09 09:23:59 -0700

    Solved Codewars 'Combine Strings' problem

---
+++ 8kyu/combineStrings/js/main.js
@@ -0,0 +1,15 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+
+function combineNames(str, str2){
+    return `${str} ${str2}`
+}
+
+console.log(combineNames('James', 'Stevens'))
