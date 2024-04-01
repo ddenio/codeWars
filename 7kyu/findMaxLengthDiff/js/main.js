c7f440ea9da50d6bf7f91dcd34e3771c5993fbeb
Derin DeNio <derindenio@gmail.com>
2024-04-01 17:53:12 -0600

    Solved Codewars 'Find Maximum Lenght Difference' problem

---
+++ 7kyu/findMaxLengthDiff/js/main.js
@@ -0,0 +1,26 @@
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any 
+// string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+// Example:
+// a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+// a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+// mxdiflg(a1, a2) --> 13
+// Bash note:
+// input : 2 strings with substrings separated by ,
+// output: number as a string
+
+function mxdiflg(a1, a2) {
+   a1 = a1.sort((a,b) => a.length-b.length)
+   a2 = a2.sort((a,b) => a.length-b.length)
+   if (a1.length === 0 || a2.length === 0){
+    return -1
+   }else if ( a2[a2.length-1].length - a1[0].length > a1[a1.length-1].length - a2[0].length) {
+    return a2[a2.length-1].length - a1[0].length
+   } return a1[a1.length-1].length - a2[0].length
+}
+
+console.log(mxdiflg(["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"], ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]))
