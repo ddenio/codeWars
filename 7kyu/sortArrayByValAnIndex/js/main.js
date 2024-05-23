4a0ce1653caee923df5d6cc1c87336a8c57d01a8
Derin DeNio <derindenio@gmail.com>
2024-05-22 18:12:25 -0600

    Solved Codewars 'Sort an array by value and index' problem

---
+++ 7kyu/sortArrayByValAnIndex/js/main.js
@@ -0,0 +1,26 @@
+// Sort an array by value and index
+// Your task is to sort an array of integer numbers by the product of the value and the index of the positions.
+
+// For sorting the index starts at 1, NOT at 0!
+// The sorting has to be ascending.
+// The array will never be null and will always contain numbers.
+
+// Example:
+
+// Input: 23, 2, 3, 4, 5
+// Product of value and index:
+// 23 => 23 * 1 = 23  -> Output-Pos 4
+//  2 =>  2 * 2 = 4   -> Output-Pos 1
+//  3 =>  3 * 3 = 9   -> Output-Pos 2
+//  4 =>  4 * 4 = 16  -> Output-Pos 3
+//  5 =>  5 * 5 = 25  -> Output-Pos 5
+
+// Output: 2, 3, 4, 23, 5
+
+function sortByValueAndIndex(array) {
+    let arr2 = array.map((e, index) => ({ value: e, product: e * (index + 1), originalIndex: index }));
+    arr2.sort((a, b) => a.product - b.product || a.originalIndex - b.originalIndex);
+    return arr2.map(obj => obj.value);
+}
+
+console.log(sortByValueAndIndex([23,2,3,4,5]))
