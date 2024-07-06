a8bf0317496cf64eca83bafb47898a7e8b8e1514
Derin DeNio <derindenio@gmail.com>
2024-07-05 21:28:07 -0600

    Solved Codewars 'A equals B' problem

---
+++ 7kyu/aEqualsB/js/main.js
@@ -0,0 +1,10 @@
+// Compare 2 integers and return if they are equals, don't use the comparison Operators
+
+// Can't use these Operators
+
+function d01(a,b) {
+    return !(a - b)
+  }
+
+console.log(d01(1,1))
+console.log(d01(1,2))
