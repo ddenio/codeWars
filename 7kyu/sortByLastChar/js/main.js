df535120bf43859af105ae3e3007ac148214ea42
Derin DeNio <derindenio@gmail.com>
2025-03-08 16:28:44 -0700

    Solved Codewars 'Sort by last char' problem

---
+++ 7kyu/sortByLastChar/js/main.js
@@ -0,0 +1,11 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+function last(x){
+    return x.split(' ').sort((a,b) => a[a.length-1].localeCompare(b[b.length-1]))
+}
+
+console.log(last("man i need a taxi up to ubud"))
