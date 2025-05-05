81974ffb5bdfcc0833f2e608bb46b44f561ac1d7
Derin DeNio <derindenio@gmail.com>
2025-05-04 19:18:44 -0600

    Solved Codewars 'incorrect div method' problem

---
+++ 8kyu/incorrectDivMethod/js/main.js
@@ -0,0 +1,9 @@
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+// const solve = (x, y) => x // y
+
+const solve = (x, y) => x / y
+
+console.log(solve(2,1))
+console.log(solve(2,4))
+console.log(solve(42,0))
