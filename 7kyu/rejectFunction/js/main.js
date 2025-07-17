9547302e2e81cbfc09dd5d24da159e80dc298ef4
Derin DeNio <derindenio@gmail.com>
2025-07-16 18:17:43 -0600

    Solved codewars 'Reject function' problem

---
+++ 7kyu/rejectFunction/js/main.js
@@ -0,0 +1,11 @@
+// Implement a function which filters out array values which satisfy the given predicate.
+
+// reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
+
+function reject(array, predicate) {
+    return array.filter(e => !predicate(e))
+}
+
+
+
+console.log(reject([1,2,3,4,5], n=> n%2 === 0))
